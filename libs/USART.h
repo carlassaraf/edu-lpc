5cb7d06d75ca129748624a9f41a48b0e6d427a6b
Fabrizio Carlassara <53102151+carlassaraf@users.noreply.github.com>
2021-11-18 22:45:53 -0300

    Updated USART.h
    See USART.cpp commit

--- libs/USART.h
+++ libs/USART.h
@@ -1,33 +1,30 @@
 /*
  * USART.h
  *
- *  Created on: Jun 10, 2021
+ *  Created on: Oct 8, 2021
  *      Author: fabri
  */
 
 #ifndef USART_H_
 #define USART_H_
 
+#include <fsl_common.h>
 #include <fsl_usart.h>
-#include <stdio.h>
-#include <string.h>
+#include <fsl_swm.h>
 
 class USART {
-
 	public:
-		USART(uint8_t n);
-		USART(uint8_t n, uint32_t baudRate);
-		virtual ~USART();
-		uint8_t read(void);
-		uint8_t* read(uint8_t length);
-		void print(const char *str);
-		void print(const char *str, float number);
-		void print(uint8_t* bytes);
-		void print(int32_t number);
-		//void print(float number);
 
-	private:
-		USART_Type* type;
+		uint32_t usart_n;
+		USART_Type *usart;
+		swm_select_movable_t swm_pins[5];	// { kSWM_USARTn_TXD, kSWM_USARTn_RXD, kSWM_USARTn_RTS, kSWM_USARTn_CTS, kSWM_USARTn_SCLK }
+
+		USART(uint32_t usart_type, uint32_t baud_rate);
+		virtual ~USART();
+
+		void prints(const uint8_t *buff);
+		void assignPins(uint8_t txd, uint8_t rxd);
+		void attachInterrupt(void (*f)(void));
 };
 
 #endif /* USART_H_ */
