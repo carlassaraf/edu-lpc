4713791219fdfe73d08e12b8d27d43d806be3942
Fabrizio Carlassara <53102151+carlassaraf@users.noreply.github.com>
2021-12-20 22:42:08 -0300

    Updated LM35 driver
    Documentation was added and support for two channel ADC was removed

--- libs/LM35.cpp
+++ libs/LM35.cpp
@@ -5,25 +5,56 @@
  *      Author: fabri
  */
 
-#include <LM35.h>
+#include "LM35.h"
 
-LM35::~LM35() {
-	// TODO Auto-generated destructor stub
-}
+/*!
+ * @brief LM35 getRaw method.
 
+ * Reads the output voltage from the LM35.
+ *
+ * @param None.
+ *
+ * @retval raw 12 bit ADC value.
+ */
 uint32_t LM35::getRaw(void) {
 
-	LM35_Data.rawTotal = v_total.read();
-	LM35_Data.rawDiodes = v_diodes.read();
+	LM35_Data.raw = lm35_output.read();
 
-	LM35_Data.rawTemperature = LM35_Data.rawTotal - LM35_Data.rawDiodes;
-	return LM35_Data.rawTemperature;
+	return LM35_Data.raw;
 }
 
+/*!
+ * @brief LM35 getTemperatureCelsius method.
+
+ * Reads the output voltage from the LM35 and converts
+ * the value to temperature in Celsius.
+ *
+ * @param None.
+ *
+ * @retval temperature in Celsius.
+ */
 float LM35::getTemperatureCelsius(void) {
 
 	getRaw();
-	LM35_Data.temperatureCelsius = 3.3 * (float) LM35_Data.rawTemperature / 4095 * 100;
+	LM35_Data.temperatureCelsius = 3.3 * (float) LM35_Data.raw / 4095 * 100;
 
 	return LM35_Data.temperatureCelsius;
+}
+
+/*!
+ * @brief LM35 getTemperatureFahrenheit method.
+
+ * Reads the output voltage from the LM35 and converts
+ * the value to temperature in Fahrenheit.
+ *
+ * @param None.
+ *
+ * @retval temperature in Fahrenheit.
+ */
+float LM35::getTemperatureFahrenheit(void) {
+
+	getTemperatureCelsius();
+	LM35_Data.temperatureFahrenheit = (LM35_Data.temperatureCelsius * 9 / 5) + 32;
+
+	return LM35_Data.temperatureFahrenheit; 
 }
--- libs/LM35.h
+++ libs/LM35.h
@@ -8,28 +8,31 @@
 #ifndef LM35_H_
 #define LM35_H_
 
+/*  LM35 external includes  */
+
 #include "ADC.h"
+
+/*  LM35 data struct  */
 
 typedef struct {
 
-	uint32_t rawTotal;
-	uint32_t rawDiodes;
-	uint32_t rawTemperature;
+	uint32_t raw;
 	float temperatureCelsius;
 	float temperatureFahrenheit;
 
 } LM35_Data_t;
 
+/*  Class definition  */
+
 class LM35 {
 
-	ADC v_total;
-	ADC v_diodes;
+	ADC lm35_output;
 
 	public:
-		LM35(void) : v_total(8), v_diodes(7) {};
-		virtual ~LM35();
+		LM35(uint32_t channel) : lm35_output(channel) {};
 		uint32_t getRaw(void);
 		float getTemperatureCelsius(void);
+		float getTemperatureFahrenheit(void);
 
 	private:
 		LM35_Data_t LM35_Data;
