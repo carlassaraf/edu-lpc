1c5e7380ea9113cdff381e59ba4d3723cc67f031
Fabrizio Carlassara <53102151+carlassaraf@users.noreply.github.com>
2021-12-24 00:26:23 -0300

    Documentation added

--- libs/I2C.cpp
+++ libs/I2C.cpp
@@ -7,16 +7,50 @@
 
 #include <I2C.h>
 
+/*!
+ * @brief I2C constructor.
+
+ * Creates an I2C object. Default frequency is 12MHz.
+ * I2C0 default pins: PIO0_10 SCL / PIO0_11 SDA.
+ * I2C1-3 default pins: PIO0_1 SCL / PIO0_0 SDA.
+ *
+ * @param i2cn is the I2C module to use (0-3).
+ *
+ * @retval None.
+ */
 I2C::I2C(uint32_t i2cn) {
 
 	init(i2cn, 12000000U);
 }
 
+/*!
+ * @brief I2C constructor.
+
+ * Creates an I2C object.
+ * I2C0 default pins: PIO0_10 SCL / PIO0_11 SDA.
+ * I2C1-3 default pins: PIO0_1 SCL / PIO0_0 SDA.
+ *
+ * @param i2cn is the I2C module to use (0-3).
+ * @param frequency the frequency of the I2C communication
+ *
+ * @retval None.
+ */
 I2C::I2C(uint32_t i2cn, uint32_t frequency) {
 
 	init(i2cn, frequency);
 }
 
+/*!
+ * @brief I2C assignPins method.
+
+ * Assigns the pins for the I2C communication. Only
+ * works for I2C1-3
+ *
+ * @param sda index of the pin to use as SDA.
+ * @param scl index of the pin to use as SCL.
+ *
+ * @retval None.
+ */
 void I2C::assignPins(uint32_t sda, uint32_t scl) {
 
 	swm_select_movable_t swm_sda, swm_scl;
@@ -40,6 +74,17 @@ void I2C::assignPins(uint32_t sda, uint32_t scl) {
 	SYSCON->SYSAHBCLKCTRL0	&= ~(SYSCON_SYSAHBCLKCTRL0_SWM_MASK);	// Disable SWM CLK
 }
 
+/*!
+ * @brief I2C write method.
+
+ * Writes a number of bytes to the slave addressed.
+ *
+ * @param address 7-bit address of the slave device.
+ * @param buff array with the bytes to be sent.
+ * @param size number of bytes to be sent.
+ *
+ * @retval status of the communication.
+ */
 status_t I2C::write(uint8_t address, uint8_t *buff, uint32_t size) {
 
 	I2C_MasterStart(i2c, address, kI2C_Write);
@@ -47,6 +92,17 @@ status_t I2C::write(uint8_t address, uint8_t *buff, uint32_t size) {
 	return I2C_MasterStop(i2c);
 }
 
+/*!
+ * @brief I2C read method.
+
+ * Reads a number of bytes from the slave addressed.
+ *
+ * @param address 7-bit address of the slave device.
+ * @param buff array to store the data.
+ * @param size number of bytes to be read.
+ *
+ * @retval status of the communication.
+ */
 status_t I2C::read(uint8_t address, uint8_t *buff, uint32_t size) {
 
 	I2C_MasterStart(i2c, address, kI2C_Read);
@@ -54,13 +110,17 @@ status_t I2C::read(uint8_t address, uint8_t *buff, uint32_t size) {
 	return I2C_MasterStop(i2c);
 }
 
-void I2C::init(uint32_t i2cn, uint32_t frequency) {
+/*!
+ * @brief I2C private init method.
 
-	/*
-	 * I2C0 default pins: PIO0_10 SCL / PIO0_11 SDA
-	 * I2C1-3 default pins: PIO0_1 SCL / PIO0_0 SDA
-	 *
-	 * */
+ * Deals with the initialization of the I2C.
+ *
+ * @param i2cn index of the I2C module to be used.
+ * @param frequency the frequency of the communication.
+ *
+ * @retval None.
+ */
+void I2C::init(uint32_t i2cn, uint32_t frequency) {
 
 	swm_select_movable_t swm_sda, swm_scl;
 
@@ -110,6 +170,15 @@ void I2C::init(uint32_t i2cn, uint32_t frequency) {
 	I2C_MasterInit(i2c, &masterConfig, frequency);
 }
 
+/*!
+ * @brief I2C private getInstance method.
+
+ * Gets the I2C module to be used and selects the clock.
+ *
+ * @param i2cn index of the I2C module to be used.
+ *
+ * @retval I2C module.
+ */
 I2C_Type* I2C::getInstance(uint32_t i2cn) {
 
 	if(i2cn == 0) {
