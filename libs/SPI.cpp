895bbee14249f6fb04e87fe5c528aff81d064fde
Fabrizio Carlassara <53102151+carlassaraf@users.noreply.github.com>
2021-12-25 03:13:18 -0300

    Fixed incopatible type casting

--- libs/SPI.cpp
+++ libs/SPI.cpp
@@ -26,18 +26,18 @@ SPI::SPI(uint32_t spin, uint32_t mosi, uint32_t miso, uint32_t sck, uint32_t sse
 
     if(spin == 0) {
     	spi = SPI0;
-        SWM_SetMovablePinSelect(SWM0, kSWM_SPI0_SCK, (swm_select_movable_t)sck);
-        SWM_SetMovablePinSelect(SWM0, kSWM_SPI0_MOSI, (swm_select_movable_t)mosi);
-        SWM_SetMovablePinSelect(SWM0, kSWM_SPI0_MISO, (swm_select_movable_t)miso);
-        SWM_SetMovablePinSelect(SWM0, kSWM_SPI0_SSEL0, (swm_select_movable_t)ssel);
+        SWM_SetMovablePinSelect(SWM0, kSWM_SPI0_SCK, (swm_port_pin_type_t)sck);
+        SWM_SetMovablePinSelect(SWM0, kSWM_SPI0_MOSI, (swm_port_pin_type_t)mosi);
+        SWM_SetMovablePinSelect(SWM0, kSWM_SPI0_MISO, (swm_port_pin_type_t)miso);
+        SWM_SetMovablePinSelect(SWM0, kSWM_SPI0_SSEL0, (swm_port_pin_type_t)ssel);
         CLOCK_Select(kSPI0_Clk_From_MainClk);
     }
     else if(spin == 1) {
     	spi = SPI1;
-        SWM_SetMovablePinSelect(SWM0, kSWM_SPI1_SCK, (swm_select_movable_t)sck);
-        SWM_SetMovablePinSelect(SWM0, kSWM_SPI1_MOSI, (swm_select_movable_t)mosi);
-        SWM_SetMovablePinSelect(SWM0, kSWM_SPI1_MISO, (swm_select_movable_t)miso);
-        SWM_SetMovablePinSelect(SWM0, kSWM_SPI1_SSEL0, (swm_select_movable_t)ssel);
+        SWM_SetMovablePinSelect(SWM0, kSWM_SPI1_SCK, (swm_port_pin_type_t)sck);
+        SWM_SetMovablePinSelect(SWM0, kSWM_SPI1_MOSI, (swm_port_pin_type_t)mosi);
+        SWM_SetMovablePinSelect(SWM0, kSWM_SPI1_MISO, (swm_port_pin_type_t)miso);
+        SWM_SetMovablePinSelect(SWM0, kSWM_SPI1_SSEL0, (swm_port_pin_type_t)ssel);
         CLOCK_Select(kSPI0_Clk_From_MainClk);
     }
 
