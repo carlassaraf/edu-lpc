fdb632235aeb49a586529e70ffa83434942dbaa9
Fabrizio Carlassara <53102151+carlassaraf@users.noreply.github.com>
2022-01-21 17:03:31 -0300

    Changed macros to constant expressions

--- libs/GPIO.h
+++ libs/GPIO.h
@@ -15,46 +15,49 @@
 #include "fsl_syscon.h"
 #include "fsl_syscon_connections.h"
 
-/* GPIO Macros */
+/* GPIO inline functions */
+inline uint8_t getPORT(uint32_t pin) {
+	return (pin < 32)? 0 : 1;
+}
 
-#define getPORT(pin)				(pin < 32)? 0 : 1
-#define getSYSCON_CONNECTION(pin)	(syscon_connection_t)(pin + (PINTSEL_ID << SYSCON_SHIFT))
-
-/* Define GPIO modes and outputs */
-
-#define INPUT	kGPIO_DigitalInput
-#define OUTPUT	kGPIO_DigitalOutput
-
-#define LOW		0
-#define HIGH	1
-
-/* PINT Modes */
-
-#define PINT_NONE			kPINT_PinIntEnableNone
-#define PINT_RISE_EDGE		kPINT_PinIntEnableRiseEdge
-#define PINT_FALL_EDGE		kPINT_PinIntEnableFallEdge
-#define PINT_BOTH_EDGES		kPINT_PinIntEnableBothEdges
-#define PINT_LOW_LEVEL		kPINT_PinIntEnableLowLevel
-#define PINT_HIGH_LEVEL		kPINT_PinIntEnableHighLevel
-
-static const gpio_pin_config_t configOutput = {
-    		kGPIO_DigitalOutput,
-			1,
-};
-
-static const gpio_pin_config_t configInput = {
-		kGPIO_DigitalInput,
-		0,
-};
+inline syscon_connection_t getSYSCON_CONNECTION(uint32_t pin) {
+	return (syscon_connection_t)(pin + (PINTSEL_ID << SYSCON_SHIFT));
+}
 
 /* Class Definition */
-
 class Pin {
 
 	public:
+		/* Class constants */
+		static constexpr uint8_t INPUT {(uint8_t)kGPIO_DigitalInput};
+		static constexpr uint8_t OUTPUT {(uint8_t)kGPIO_DigitalOutput};
 
+		static constexpr uint8_t LOW {0};
+		static constexpr uint8_t HIGH {1};
+
+		/* Configuration constants */
+		static constexpr gpio_pin_config_t configOutput = {
+		    		kGPIO_DigitalOutput,
+					1,
+		};
+
+		static constexpr gpio_pin_config_t configInput = {
+				kGPIO_DigitalInput,
+				0,
+		};
+
+		/* PINT related constants */
+		static constexpr pint_pin_enable_t PINT_NONE = kPINT_PinIntEnableNone;
+		static constexpr pint_pin_enable_t PINT_RISE_EDGE = kPINT_PinIntEnableRiseEdge;
+		static constexpr pint_pin_enable_t PINT_FALL_EDGE = kPINT_PinIntEnableFallEdge;
+		static constexpr pint_pin_enable_t PINT_BOTH_EDGES = kPINT_PinIntEnableBothEdges;
+		static constexpr pint_pin_enable_t PINT_LOW_LEVEL = kPINT_PinIntEnableLowLevel;
+		static constexpr pint_pin_enable_t PINT_HIGH_LEVEL = kPINT_PinIntEnableHighLevel;
+
+		/* Constructors */
 		Pin(uint32_t pin_index, const uint32_t mode);
 		Pin(uint32_t pin_index, const uint32_t mode, const uint32_t initial_value);
+		/* Public methods */
 		uint32_t read();
 		void write(const uint32_t value);
 		void set(void);
@@ -64,9 +67,11 @@ class Pin {
 
 
 	private:
+		/* Index of the port associated with the Pin */
 		uint8_t port;
+		/* Pin index 0-31 */
 		uint8_t	pin;
-
+		/* General initialization private method */
 		void init(uint32_t pin_index, const uint32_t mode);
 };
 
