e8d065efaa15c4e65083bf57b2a3b5ba7e7f0b7e
Fabrizio Carlassara <53102151+carlassaraf@users.noreply.github.com>
2022-01-22 20:15:22 -0300

    Added configuration struct and static members

--- libs/PWM.cpp
+++ libs/PWM.cpp
@@ -7,22 +7,12 @@
 
 #include <PWM.h>
 
-SCTimer *sctimer = new SCTimer();	// SCTimer object for every PWM channel
-
-/*!< @brief Keep track of SCTimer event number */
-static uint32_t s_currentEvent = 0;
-
-/*!< @brief Keep track of SCTimer unify 32-bit or low 16-bit match/capture register number. */
-static uint32_t s_currentMatch = 0;
-
-/*!< @brief Keep track of SCTimer high 16-bit match/capture register number. */
-static uint32_t s_currentMatchhigh = 0;
-
-/*!< @brief Keep track of SCTimer state number */
-static uint32_t s_currentState = 0;
-
-/*!< @brief Keep track of SCTimer output number */
-static sctimer_out_t s_currentOutput = kSCTIMER_Out_0;
+/* SCTimer object for every PWM channel */
+SCTimer *sctimer = new SCTimer();
+/* Initialize static members */
+uint8_t PWM::s_currentEvent = 0;
+uint8_t PWM::s_currentMatch = 0;
+sctimer_out_t PWM::s_currentOutput = kSCTIMER_Out_0;
 
 /*!
  * @brief PWM constructor.
@@ -35,22 +25,17 @@ static sctimer_out_t s_currentOutput = kSCTIMER_Out_0;
  *
  * @retval None.
  */
-PWM::PWM(uint32_t pwm_output, uint32_t frequency, uint32_t outputPin, uint32_t duty) {
-
-	pwmParams.output = s_currentOutput;
-	pwmParam.level = kSCTIMER_HighTrue;
-	pwmParam.dutyCyclePercent = duty;
-
-	pwmFrequency = frequency;
-
+PWM::PWM(uint8_t outputPin, uint32_t frequency, uint8_t duty) {
+	/* Update settings with the user arguments */
+	settings.outputPin = outputPin;
+	settings.config.output = s_currentOutput;
+	settings.config.dutyCyclePercent = duty;
 	/* Increment SCT Output for the next PWM */
 	int out = (int)s_currentOutput;
 	s_currentOutput = (sctimer_out_t)(out + 1);
-    
+    /* Configure output pin */
 	setOutputPin(outputPin);
-
-	timer = sctimer;
-
+	/* Initialize PWM with the current settings */
 	init(frequency);
 }
 
@@ -63,9 +48,9 @@ PWM::PWM(uint32_t pwm_output, uint32_t frequency, uint32_t outputPin, uint32_t d
  *
  * @retval None.
  */
-void PWM::setDuty(uint32_t duty) {
+void PWM::setDuty(uint8_t duty) {
 
-	SCTIMER_UpdatePwmDutycycle(timer->getSCTimer(), pwmParam.output, duty, event);
+	SCTIMER_UpdatePwmDutycycle(SCT0, settings.config.output, duty, event);
 }
 
 /*!
@@ -77,9 +62,9 @@ void PWM::setDuty(uint32_t duty) {
  * 
  * @retval PWM duty cycle in percentage.
  */
-uint32_t PWM::getDuty(void) {
+uint8_t PWM::getDuty(void) {
 
-	return pwmParam.dutyCyclePercent;
+	return settings.config.dutyCyclePercent;
 }
 
 /*!
@@ -98,7 +83,7 @@ void PWM::setFrequency(uint32_t frequency) {
 	/* Overwrite the original PWM values */
 	s_currentEvent = periodEvent;
 	s_currentMatch = periodEvent;
-	init(frequency, pwmParam.level);
+	init(frequency, settings.config.level);
 }
 
 /*!
@@ -128,7 +113,7 @@ uint32_t PWM::getFrequency(void) {
  */
 void PWM::logicLevel(bool logic) {
 
-	pwmParams.level = (sctimer_pwm_level_select_t)logic;
+	settings.config.level = (sctimer_pwm_level_select_t)logic;
 	init(pwmFrequency);
 }
 
@@ -141,10 +126,10 @@ void PWM::logicLevel(bool logic) {
  *
  * @retval None.
  */
-void PWM::setOutputPin(uint32_t pin) {
+void PWM::setOutputPin(uint8_t pin) {
 
     CLOCK_EnableClock(kCLOCK_Swm);
-    SWM_SetMovablePinSelect(SWM0, (swm_select_movable_t)(pwmParams.output + kSWM_SCT_OUT0), (swm_port_pin_type_t)pin);
+    SWM_SetMovablePinSelect(SWM0, (swm_select_movable_t)(settings.config.output + kSWM_SCT_OUT0), (swm_port_pin_type_t)pin);
     CLOCK_DisableClock(kCLOCK_Swm);
 }
 
@@ -162,17 +147,15 @@ void PWM::init(uint32_t frequency, bool logic) {
 	if(pwmFrequency != frequency) {
 		pwmFrequency = frequency;
 	}
-
-	SCT_Type *sc = timer->getSCTimer();
 	/* Stop counter before configuring another PWM */
-	SCTIMER_StopTimer(sc, kSCTIMER_Counter_U);
+	SCTIMER_StopTimer(SCT0, kSCTIMER_Counter_U);
 
     /* If we do not have enough events available (this function will create two events),
      * the function will return fail.
      */
     uint32_t period, pulsePeriod = 0;
     uint32_t sctimerClock = CLOCK_GetFreq(kCLOCK_Fro);
-    uint32_t sctClock    = sctimerClock / (((sc->CTRL & SCT_CTRL_PRE_L_MASK) >> SCT_CTRL_PRE_L_SHIFT) + 1U);
+    uint32_t sctClock    = sctimerClock / (((SCT0->CTRL & SCT_CTRL_PRE_L_MASK) >> SCT_CTRL_PRE_L_SHIFT) + 1U);
     uint32_t reg;
 
     if ((s_currentEvent + 2U) > (uint32_t)FSL_FEATURE_SCT_NUMBER_OF_EVENTS) {
@@ -180,7 +163,7 @@ void PWM::init(uint32_t frequency, bool logic) {
     }
 
 	/* Use bi-directional mode for center-aligned PWM */
-	sc->CTRL |= SCT_CTRL_BIDIR_L_MASK;
+	SCT0->CTRL |= SCT_CTRL_BIDIR_L_MASK;
 
 	/* Calculate PWM period match value */
 	period = sctClock / (frequency * 2U);
@@ -191,9 +174,9 @@ void PWM::init(uint32_t frequency, bool logic) {
 	 * results in 100% dutycyle.
 	 */
 
-	pulsePeriod = (uint32_t)(((uint64_t)period * pwmParams.dutyCyclePercent) / 100U);
+	pulsePeriod = (uint32_t)(((uint64_t)period * settings.config.dutyCyclePercent) / 100U);
 
-	if (pwmParams.dutyCyclePercent >= 100U) {
+	if (settings.config.dutyCyclePercent >= 100U) {
 		pulsePeriod = period - 1U;
 	}
 
@@ -205,24 +188,24 @@ void PWM::init(uint32_t frequency, bool logic) {
 	SCTIMER_SetupCounterLimitAction(sc, kSCTIMER_Counter_U, periodEvent);
 
 	/* For high-true level */
-	if ((uint32_t)pwmParams.level == (uint32_t)kSCTIMER_HighTrue) {
+	if ((uint32_t)settings.config.level == (uint32_t)kSCTIMER_HighTrue) {
 		/* Set the initial output level to low which is the inactive state */
-		sc->OUTPUT &= ~(1UL << (uint32_t)pwmParams.output);			/* Clear the output when we reach the PWM pulse event */
-		SCTIMER_SetupOutputClearAction(sc, (uint32_t)pwmParams.output, pulseEvent);
+		SCT0->OUTPUT &= ~(1UL << (uint32_t)settings.config.output);			/* Clear the output when we reach the PWM pulse event */
+		SCTIMER_SetupOutputClearAction(sc, (uint32_t)settings.config.output, pulseEvent);
 	}
 	/* For low-true level */
 	else {
 		/* Set the initial output level to high which is the inactive state */
-		sc->OUTPUT |= (1UL << (uint32_t)pwmParams.output);
+		SCT0->OUTPUT |= (1UL << (uint32_t)settings.config.output);
 		/* Set the output when we reach the PWM pulse event */
-		SCTIMER_SetupOutputSetAction(sc, (uint32_t)pwmParams.output, pulseEvent);
+		SCTIMER_SetupOutputSetAction(sc, (uint32_t)settings.config.output, pulseEvent);
 	}
 
 	/* Reverse output when down counting */
-	reg = sc->OUTPUTDIRCTRL;
-	reg &= ~((uint32_t)SCT_OUTPUTDIRCTRL_SETCLR0_MASK << (2U * (uint32_t)pwmParams.output));
-	reg |= (1UL << (2U * (uint32_t)pwmParams.output));
-	sc->OUTPUTDIRCTRL = reg;
+	reg = SCT0->OUTPUTDIRCTRL;
+	reg &= ~((uint32_t)SCT_OUTPUTDIRCTRL_SETCLR0_MASK << (2U * (uint32_t)settings.config.output));
+	reg |= (1UL << (2U * (uint32_t)settings.config.output));
+	SCT0->OUTPUTDIRCTRL = reg;
 
 	/* Start Timer counter again */
 	SCTIMER_StartTimer(sc, kSCTIMER_Counter_U);
@@ -237,34 +220,31 @@ void PWM::init(uint32_t frequency, bool logic) {
  *
  * @param matchValue value to be reached by the counter and trigger
  * the event.
- * @param whichCounter whether it is a 16 bit counter, lower or high; or
- * the unified 32 bit counter.
  *
  * @retval event index.
  */
-uint32_t PWM::createEvent(uint32_t matchValue, sctimer_counter_t whichCounter) {
+uint8_t PWM::createEvent(uint32_t matchValue) {
 
-	SCT_Type *sc = timer->getSCTimer();
     uint32_t currentCtrlVal = (uint32_t)kSCTIMER_MatchEventOnly;
 
     if (s_currentEvent < (uint32_t)FSL_FEATURE_SCT_NUMBER_OF_EVENTS) {
 
-		if ((kSCTIMER_Counter_L == whichCounter) && (0U == (sc->CONFIG & SCT_CONFIG_UNIFY_MASK))) {
+		if ((kSCTIMER_Counter_L == whichCounter) && (0U == (SCT0->CONFIG & SCT_CONFIG_UNIFY_MASK))) {
 
 			if (s_currentMatch < (uint32_t)FSL_FEATURE_SCT_NUMBER_OF_MATCH_CAPTURE) {
 
 				currentCtrlVal |= SCT_EV_CTRL_MATCHSEL(s_currentMatch);
 
 				/* Use Counter_L bits if user wants to setup the Low counter */
-				sc->MATCH_ACCESS16BIT[s_currentMatch].MATCHL       = (uint16_t)matchValue;
-				sc->MATCHREL_ACCESS16BIT[s_currentMatch].MATCHRELL = (uint16_t)matchValue;
-				sc->EV[s_currentEvent].CTRL                        = currentCtrlVal;
+				SCT0->MATCH_ACCESS16BIT[s_currentMatch].MATCHL       = (uint16_t)matchValue;
+				SCT0->MATCHREL_ACCESS16BIT[s_currentMatch].MATCHRELL = (uint16_t)matchValue;
+				SCT0->EV[s_currentEvent].CTRL                        = currentCtrlVal;
 
 				/* Increment the match register number */
 				s_currentMatch++;
 			}
 		}
-		else if ((kSCTIMER_Counter_H == whichCounter) && (0U == (sc->CONFIG & SCT_CONFIG_UNIFY_MASK))) {
+		else if ((kSCTIMER_Counter_H == whichCounter) && (0U == (SCT0->CONFIG & SCT_CONFIG_UNIFY_MASK))) {
 
 			if (s_currentMatchhigh < (uint32_t)FSL_FEATURE_SCT_NUMBER_OF_MATCH_CAPTURE) {
 
@@ -272,24 +252,24 @@ uint32_t PWM::createEvent(uint32_t matchValue, sctimer_counter_t whichCounter) {
 
 				/* Use Counter_H bits if user wants to setup the High counter */
 				currentCtrlVal |= SCT_EV_CTRL_HEVENT(1U);
-				sc->MATCH_ACCESS16BIT[s_currentMatchhigh].MATCHH       = (uint16_t)matchValue;
-				sc->MATCHREL_ACCESS16BIT[s_currentMatchhigh].MATCHRELH = (uint16_t)matchValue;
+				SCT0->MATCH_ACCESS16BIT[s_currentMatchhigh].MATCHH       = (uint16_t)matchValue;
+				SCT0->MATCHREL_ACCESS16BIT[s_currentMatchhigh].MATCHRELH = (uint16_t)matchValue;
 
-				sc->EV[s_currentEvent].CTRL = currentCtrlVal;
+				SCT0->EV[s_currentEvent].CTRL = currentCtrlVal;
 				/* Increment the match register number */
 				s_currentMatchhigh++;
 			}
 		}
-		else if ((kSCTIMER_Counter_U == whichCounter) && (0U != (sc->CONFIG & SCT_CONFIG_UNIFY_MASK))) {
+		else if ((kSCTIMER_Counter_U == whichCounter) && (0U != (SCT0->CONFIG & SCT_CONFIG_UNIFY_MASK))) {
 
 			if (s_currentMatch < (uint32_t)FSL_FEATURE_SCT_NUMBER_OF_MATCH_CAPTURE) {
 
 				/* Use Counter_L bits if counter is operating in 32-bit mode */
 				currentCtrlVal |= SCT_EV_CTRL_MATCHSEL(s_currentMatch);
 
-				sc->MATCH[s_currentMatch]    = matchValue;
-				sc->MATCHREL[s_currentMatch] = matchValue;
-				sc->EV[s_currentEvent].CTRL  = currentCtrlVal;
+				SCT0->MATCH[s_currentMatch]    = matchValue;
+				SCT0->MATCHREL[s_currentMatch] = matchValue;
+				SCT0->EV[s_currentEvent].CTRL  = currentCtrlVal;
 
 				/* Increment the match register number */
 				s_currentMatch++;
@@ -297,7 +277,7 @@ uint32_t PWM::createEvent(uint32_t matchValue, sctimer_counter_t whichCounter) {
 		}
 	}
 	/* Enable the event in the current state */
-	sc->EV[s_currentEvent].STATE = (1UL << s_currentState);
+	SCT0->EV[s_currentEvent].STATE = (1UL << s_currentState);
 
 	/* Increment the event number */
 	s_currentEvent++;
--- libs/PWM.h
+++ libs/PWM.h
@@ -8,37 +8,56 @@
 #ifndef PWM_H_
 #define PWM_H_
 
-/*  PWM includes  */
-
+/* PWM includes */
 #include "SCTimer.h"
 #include <fsl_swm.h>
 
-/*  Class definition  */
+/* Configuration struct */
+typedef struct {
+	/* PWM configuration struct */
+	sctimer_pwm_signal_param_t config = {
+		kSCTIMER_Out_0,			/* Defaults to the current output */
+		kSCTIMER_HighTrue,		/* High level PWM */
+		50U						/* 50% duty cycle */
+	};
+	/* PWM frequency */
+	uint32_t frequency {1000U};	/* Defauls to 1KHz */
+	/* PWM output pin */
+	uint8_t outputPin {0U};		/* Defaults to 0 */
+} pwm_config_t;
 
+/* Class definition */
 class PWM {
 
 	public:
-
-		PWM(uint32_t outputPin, uint32_t frequency = 1000, uint32_t duty = 50);
-		void setDuty(uint32_t duty);
-		uint32_t getDuty(void);
+		/* Static class members */
+		/* Keep track of SCTimer event number */
+		static uint8_t s_currentEvent;
+		/* Keep track of SCTimer unify 32-bit or low 16-bit match/capture register number */
+		static uint8_t s_currentMatch;
+		/* Keep track of SCTimer unify 32-bit or low 16-bit match/capture register number */
+		static sctimer_out_t s_currentOutput;
+		/* Only supports state 0 */
+		static constexpr uint8_t s_currentState {0};
+		/* Constructors */
+		PWM(uint8_t outputPin, uint32_t frequency = 1000, uint8_t duty = 50);
+		void setDuty(uint8_t duty);
 		void setFrequency(uint32_t frequency);
-		uint32_t getFrequency(void);
 		void logicLevel(bool logic);
-		void setOutputPin(uint32_t pin);
+		void setOutputPin(uint8_t pin);
+		uint8_t getDuty(void);
+		uint32_t getFrequency(void);
 
 	private:
-
-		sctimer_pwm_signal_param_t pwmParams;
-
-		uint32_t pwmFrequency;
+		/* Settings struct to keep track of configuration */
+		pwm_config_t settings;
+		/* Instance assigned period event */
 		uint32_t periodEvent;
+		/* Instance assigned pulse event */
 		uint32_t pulseEvent;
-
-		SCTimer *timer;
-
+		/* Private methods */
 		void init(uint32_t frequency);
-		uint32_t createEvent(uint32_t matchValue, sctimer_counter_t whichCounter);
+		uint8_t createEvent(uint32_t matchValue);
 };
 
 #endif /* PWM_H_ */
