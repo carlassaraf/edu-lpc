fc12961f6816e96d409260c5beb72af657b0c45e
Fabrizio Carlassara <53102151+carlassaraf@users.noreply.github.com>
2022-01-22 20:25:07 -0300

    Class simplified

--- libs/SCTimer.h
+++ libs/SCTimer.h
@@ -8,18 +8,44 @@
 #ifndef SCTIMER_H_
 #define SCTIMER_H_
 
+/* SCTimer includes */
 #include "fsl_sctimer.h"
 
+/* Class definition */
 class SCTimer {
 
 	public:
-
-		SCTimer(uint32_t index = 0);
-		SCT_Type* getSCTimer(void);
-
-	private:
-
-		SCT_Type *timer;
+		SCTimer(void);
 };
+
+/* Inline methods */
+
+/*!
+ * @brief SCTimer constructor.
+
+ * Creates a SCTimer object. Default timmer is SCT0.
+ *
+ * @param index SCTimer index.
+ *
+ * @retval None.
+ */
+inline SCTimer::SCTimer(void) {
+	/* Default configuration */
+	sctimer_config_t config {
+		true,							/* Enable counter unify */
+		kSCTIMER_System_ClockMode,		/* System clock */
+		kSCTIMER_Clock_On_Rise_Input_0,	/* Clock selected */
+		false, 							/* Up count Counter L */
+		false,							/* Up count Counter H */
+		0U,								/* Prescale Counter L */
+		0U,								/* Prescale Counter H */
+		0U,								/* Initial output value */
+		0xFU							/* Input synchronization */
+	};
+	/* Reset any prevous SCTimer configuration */
+	SCTIMER_Deinit(SCT0);
+	/* Initialize SCTimer with defuault config */
+	SCTIMER_Init(SCT0, &config);
+}
 
 #endif /* SCTIMER_H_ */
