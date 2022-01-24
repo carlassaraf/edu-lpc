e5e763cf97a38a289665ccffbb2e62d8fafd5fdd
Fabrizio Carlassara <53102151+carlassaraf@users.noreply.github.com>
2022-01-24 01:08:35 -0300

    Simplified LM35 class

--- libs/LM35.h
+++ libs/LM35.h
@@ -8,34 +8,103 @@
 #ifndef LM35_H_
 #define LM35_H_
 
-/*  LM35 external includes  */
-
+/* LM35 external includes */
 #include "ADC.h"
 
-/*  LM35 data struct  */
-
+/* LM35 data struct */
 typedef struct {
+	/* 12-bit raw value */
+	uint16_t raw;
+	/* Temperature in Celsius */
+	float tempC;
+	/* Temperature in Fahrenheit */
+	float tempF;
+} lm35_data_t;
 
-	uint32_t raw;
-	float temperatureCelsius;
-	float temperatureFahrenheit;
-
-} LM35_Data_t;
-
-/*  Class definition  */
-
+/* Class definition */
 class LM35 {
 
-	ADC lm35_output;
-
 	public:
-		LM35(uint32_t channel) : lm35_output(channel) {};
-		uint32_t getRaw(void);
-		float getTemperatureCelsius(void);
-		float getTemperatureFahrenheit(void);
+		/* Constructors */
+		LM35(uint8_t channel = 1);
+		/* Public methods */
+		void update(void);
+		uint16_t getRaw(void);
+		float getTempC(void);
+		float getTempF(void);
 
 	private:
-		LM35_Data_t LM35_Data;
+		/* ADC object pointer */
+		ADC *adc;
+		/* LM35 data variable */
+		lm35_data_t data;
 };
+
+/*!
+ * @brief LM35 constructor.
+
+ * Creates a LM35 object.
+ *
+ * @param channel index of the ADC channel
+ * to be used. Defaults to 1.
+ *
+ * @retval None.
+ */
+inline LM35::LM35(uint8_t channel) {
+	/* Create a pointer to the ADC instance */
+	adc = new ADC(channel);
+}
+
+/*!
+ * @brief LM35 update method.
+
+ * Reads the output voltage from the LM35
+ * and update the data struct.
+ *
+ * @param None.
+ *
+ * @retval None.
+ */
+inline void LM35::update(void) {
+	/* Read and update raw value */
+	data.raw = adc->read();
+	/* Calculate temperature value in Celsius */
+	data.tempC = 3.3 * (float) data.raw / 4095 * 100;
+	/* Calculate temperature value in Fahrenheit */
+	data.tempF = (data.tempC * 9 / 5) + 32;
+}
+
+/*!
+ * @brief LM35 getRaw method.
+
+ * Returns the stored raw value from ADC.
+ *
+ * @param None.
+ *
+ * @retval raw 12 bit ADC value.
+ */
+inline uint16_t LM35::getRaw(void) { return data.raw; }
+
+/*!
+ * @brief LM35 getTempC method.
+
+ * Returns the stored temperature in Celsius.
+ *
+ * @param None.
+ *
+ * @retval temperature in Celsius.
+ */
+inline float LM35::getTempC(void) { return data.tempC; }
+
+/*!
+ * @brief LM35 getTempF method.
+
+ * Returns the stored temperature in Fahrenheit.
+ *
+ * @param None.
+ *
+ * @retval temperature in Fahrenheit.
+ */
+inline float LM35::getTempF(void) {	return data.tempF; }
 
 #endif /* LM35_H_ */
