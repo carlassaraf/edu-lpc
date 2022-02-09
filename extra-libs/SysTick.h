4020c30612c1e0b10115a098bb6ce572190d8737
Fabrizio Carlassara <53102151+carlassaraf@users.noreply.github.com>
2022-02-09 18:56:11 -0300

    Added set, start, stop and reset methods

--- extra-libs/SysTick.h
+++ extra-libs/SysTick.h
@@ -8,7 +8,7 @@
 #ifndef SYSTICK_H_
 #define SYSTICK_H_
 
-/* SysTick includes */
+/* Includes */
 #include "LPC845.h"
 
 /* Class definition */
@@ -17,6 +17,68 @@ class Tick {
 	public:
 		/* Constructors */
 		Tick(uint32_t us, void (*f)(void));
+		/* Public methods */
+		void set(uint32_t us);
+		void stop(void);
+		void start(void);
+		void reset(void);
 };
+
+/* Inline methods */
+
+/*!
+ * @brief Tick set method.
+
+ * Sets a new time for the interrupt.
+ *
+ * @param us time in microseconds to set the interrupt.
+ *
+ * @retval None.
+ */
+inline void Tick::set(uint32_t us) {
+	/* Stop the counter */
+	stop();
+	/* Calculate the number of ticks to match */
+	uint32_t ticks = SystemCoreClock * (us / 1E6);
+	/* Configure SysTick */
+	SysTick->LOAD = ticks - 1;
+	/* Reset counter */
+	reset();
+	/* Start counter */
+	start();
+}
+
+/*!
+ * @brief Tick stop method.
+
+ * Stops the SysTick counter.
+ *
+ * @param None.
+ *
+ * @retval None.
+ */
+inline void Tick::stop(void) { SysTick->CTRL &= ~SysTick_CTRL_ENABLE_Msk; }
+
+/*!
+ * @brief Tick start method.
+
+ * Starts the SysTick counter.
+ *
+ * @param None.
+ *
+ * @retval None.
+ */
+inline void Tick::start(void) {	SysTick->CTRL |= SysTick_CTRL_ENABLE_Msk; }
+
+/*!
+ * @brief Tick stop method.
+
+ * Resets the SysTick counter.
+ *
+ * @param None.
+ *
+ * @retval None.
+ */
+inline void Tick::reset(void) { SysTick->VAL = 0; }
 
 #endif /* SYSTICK_H_ */
